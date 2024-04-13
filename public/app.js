e2d75928b10166115b13a0eb38c55ea92888b7f4
Hugh Shin <hughbee135@gmail.com>
2024-04-12 22:25:37 -0700

    Extract client-side config into separate file

--- public/app.js
+++ public/app.js
@@ -1,28 +1,15 @@
 {
-  let titleBase = 'App';
-  let titles = {
-    '/404': '404',
-    '/a': 'A',
-    '/b': 'B'
-  };
-  let pageElementIds = {
-    '/404': 'page_404',
-    '/': 'page_root',
-    '/a': 'page_a',
-    '/b': 'page_b'
-  };
-
-  if (window.location.pathname in titles) {
-    document.title = titleBase + ': ' + titles[window.location.pathname];
+  if (window.location.pathname in config.titles) {
+    document.title = config.titleBase + ': ' + config.titles[window.location.pathname];
   } else {
-    document.title = titleBase;
+    document.title = config.titleBase;
   }
 
   for (let element of document.getElementsByClassName('page')) {
     element.style.display = 'none';
   };
-  if (window.location.pathname in pageElementIds) {
-    let element = document.getElementById(pageElementIds[window.location.pathname]);
+  if (window.location.pathname in config.pageElementIds) {
+    let element = document.getElementById(config.pageElementIds[window.location.pathname]);
     if (element) {
       element.style.display = null;
     }
---
+++ public/config.js
@@ -0,0 +1,14 @@
+const config = {
+  'titleBase': 'App',
+  'titles': {
+    '/404': '404',
+    '/a': 'A',
+    '/b': 'B'
+  },
+  'pageElementIds': {
+    '/404': 'page_404',
+    '/': 'page_root',
+    '/a': 'page_a',
+    '/b': 'page_b'
+  }
+};
